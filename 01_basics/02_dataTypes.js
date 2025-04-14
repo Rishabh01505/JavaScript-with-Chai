766a7661b05c31e190b5f33a5c4c9597522d8da7
Rishabh Bhatnagar <86870949+Rishabh01505@users.noreply.github.com>
2025-04-14 17:27:09 +0000

    latest commit

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,27 +2,23 @@
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high
+console.log(3 +
+     3); // code readability should be high
 
 console.log("Hitesh")
 
-
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
 let state;
-
+//ecma (tc39) is javascript documentation, you can use mdn too.
 // number => 2 to power 53
 // bigint
 // string => ""
 // boolean => true/false
-// null => standalone value
-// undefined => 
-// symbol => unique
-
-
+// null => standalone value (empty value)
+// undefined => (not defined)
+// symbol => unique 
 // object
 
 console.log(typeof undefined); // undefined
