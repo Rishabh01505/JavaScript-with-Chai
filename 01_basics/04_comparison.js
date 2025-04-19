cff16bb3861edf032fd78308a81784403045da02
Rishabh Bhatnagar <86870949+Rishabh01505@users.noreply.github.com>
2025-04-19 14:55:18 +0000

    latest commit

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -8,14 +8,16 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+//no problem till we compare same data types
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(null > 0); //true
+console.log(null == 0); //false
+console.log(null >= 0); //true
+//equality and comparisons work differently. comparisons convert null to 0 treating it as a number
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
 
-// === 
+// === , strict check, data type and value
 
-console.log("2" === 2);
+console.log("2" === 2); //false
