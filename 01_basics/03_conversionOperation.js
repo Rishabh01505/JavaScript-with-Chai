aebf18e1740b07ab8fbdbbf9efb2a342454e0424
Rishabh Bhatnagar <86870949+Rishabh01505@users.noreply.github.com>
2025-04-19 14:47:33 +0000

    latest commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,7 +31,7 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+// console.log(negValue); = -3
 
 // console.log(2+2);
 // console.log(2-2);
@@ -46,15 +46,15 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); = 12
+// console.log(1 + "2"); = 12
+// console.log("1" + 2 + 2); = 122 (string first converted all to string)
+// console.log(1 + 2 + "2"); = 32 (string last hence not converted)
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); = 1
+// console.log(+""); = 0
 
 let num1, num2, num3
 
